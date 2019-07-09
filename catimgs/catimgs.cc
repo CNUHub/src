da924dfd28e7342f9695767caa6e18a2e0bbdc49
Joel T. Lee <leexx235@gmail.com>
2019-07-09 12:50:39 -0500

    added option to fix voxel resolution when concatenating images with differing res

--- catimgs/catimgs.cc
+++ catimgs/catimgs.cc
@@ -1,3 +1,4 @@
+#include <float.h>
 #include "psyhdr.h"
 
 int main(int argc, char *argv[])
@@ -10,7 +11,7 @@ int main(int argc, char *argv[])
   psyimg *firstimgptr=NULL;
   psyimg *psyimgptr=NULL;
   int numinputfiles=0;
-  double scalefactor;
+  int fixvoxelscaling=0;;
   double threshold= -1e10;
   int catdim=2;
   int inputted_res=0;
@@ -18,6 +19,9 @@ int main(int argc, char *argv[])
   psytype outtype=psynotype; // will default to first input type
   psyfileclass outfileclass=psynoclass; // will default to 1st in file format
   psyorient orientation=psynoorient;  // jtlee 04/03/13 changed to default to 1st input file format
+  double filegreatestwordres=0;
+  double fileleastwordres=DBL_MAX;
+
 
 // parse input parameters
   psytype parsedouttype=psynotype;
@@ -26,6 +30,7 @@ int main(int argc, char *argv[])
     if(strcmp(argv[i], "-help") == 0 || argc<4) {
       cout<<"Usage: "<<argv[0];
       cout<<" [-dim x|y|z|i (z)] [-res dimres]\n";
+      cout<<" [-fixvoxelscaling]\n";
       cout<<" infile1 infile2 [infile3 [...]]";
       cout<<" -o outfile\n";
       cout<<" [-tran | -sag | -cor]\n";
@@ -63,8 +68,7 @@ int main(int argc, char *argv[])
       }
     }
     else if(strcmp("-res", argv[i])==0 && ((i+1)<argc)) {
-      if(inputted_res||(sscanf(argv[++i],"%lf", &dim_res)!=1))
-      {
+      if(inputted_res||(sscanf(argv[++i],"%lf", &dim_res)!=1)) {
 	cerr<<argv[0]<<": invalid dimres="<<argv[i]<<'\n';
 	exit(1);
       }
@@ -73,12 +77,16 @@ int main(int argc, char *argv[])
     else if((strcmp("-o", argv[i])==0) && ((i+1)<argc)){
       outfile=argv[++i];
     }
+    else if(strcmp("-fixvoxelscaling", argv[i])==0) fixvoxelscaling=1;
     else {
       numinputfiles++;
 // open and link each input file as we come across it
       psyfileclass infileclass;
       psytype intype;
       newinputfile=psynewinfile(argv[i], &infileclass, &intype);
+      double newinputwres=newinputfile->getwordres();
+      if(fabs(newinputwres) > fabs(filegreatestwordres)) filegreatestwordres=newinputwres;
+      else if(fabs(newinputwres) <  fabs(fileleastwordres)) fileleastwordres = newinputwres;
 // first image file
       if(firstimgptr == NULL) {
 	firstimgptr=newinputfile;
@@ -90,6 +98,56 @@ int main(int argc, char *argv[])
       }
       else {
 // cat it onto previous images
+// check for possible word resolution(voxel value scaling) problems
+	double currentwres=psyimgptr->getwordres();
+	if(fabs(currentwres - newinputwres) > 1e-16) {
+	  cout<<"word resolution varies between input files--old res="<<currentwres<<" new res="<<newinputwres<<"\n";
+	  if(! fixvoxelscaling)	cout<<"try option \"-fixvoxelscaling\" to correct\n";
+	  else {
+	    double currentfactor = 1.0;
+	    double newinputfactor = 1.0;
+	    int fixcurrent = 0;
+	    int fixnewinput = 0;
+	    psytype storetype = psynotype;
+	    // scale to 1 or choose largest factor of inputs and scale other image to same factor losing some resolution
+	    if(outtype == psydouble || outtype == psyfloat) {
+	      // change both word resolutions to 1
+	      if(fabs(currentwres - 1.0) > 1e-16) {
+		fixcurrent = 1; currentfactor = currentwres; storetype = outtype;
+	      }
+	      if(fabs(newinputwres - 1.0) > 1e-16) {
+		fixnewinput = 1; newinputfactor = newinputwres; storetype = outtype;
+	      }
+	    }
+	    else if(outfileclass == ecatfileclass) {
+	      // change both internal word resolutions to 1 & store as double -- ecat stores a seperate word res for each slice on output
+	      if(fabs(currentwres - 1.0) > 1e-16) {
+		fixcurrent = 1; currentfactor = currentwres; storetype = psydouble;
+	      }
+	      if(fabs(newinputwres - 1.0) > 1e-16) {
+		fixnewinput = 1; newinputfactor = newinputwres; storetype = psydouble;
+	      }
+	    }
+	    else if(fabs(currentwres) >= fabs(newinputwres)) {
+	      fixnewinput = 1; newinputfactor = newinputwres/currentwres; storetype = outtype;
+	      if(newinputfactor < 0 && !istypesigned(outtype)) {
+		cerr<<"can not fix negative word resolution change with unsigned data type\n";
+		cerr<<"try setting output to signed data type\n";
+		exit(1);
+	      }
+	    }
+	    else {
+	      fixcurrent = 1; currentfactor=currentwres/newinputwres; storetype = outtype;
+	      if(currentfactor < 0 && !istypesigned(outtype)) {
+		cerr<<"can not fix negative word resolution change with unsigned data type\n";
+		cerr<<"try setting output to signed data type\n";
+		exit(1);
+	      }
+	    }
+	    if(fixcurrent) psyimgptr=new scaleimg(new psypgbuff(psyimgptr), storetype, currentfactor);
+	    if(fixnewinput) newinputfile=new scaleimg(new psypgbuff(newinputfile), storetype, newinputfactor);
+	  }
+	}
 	if(inputted_res)
 	  concatimgptr=new concatimgs(psyimgptr, newinputfile, catdim, dim_res);
 	else concatimgptr=new concatimgs(psyimgptr, newinputfile, catdim);
@@ -138,6 +196,7 @@ int main(int argc, char *argv[])
   cout<<"output image min="<<min<<" max="<<max<<" mean="<<mean<<'\n';
 
 // clean up
+/* removed because prone to segmentation faults -- since stand alone main let system deal with resource clean up after process ends
   if(outpsyimgptr != NULL)delete outpsyimgptr;
   if(newpsypgbuff != NULL) delete newpsypgbuff;
   if(concatimgptr != NULL) {
@@ -153,4 +212,5 @@ int main(int argc, char *argv[])
     delete firstconcatimgptr;
   }
   else if(firstimgptr != NULL) delete firstimgptr; // only one input file no concatenation
+*/
 }
